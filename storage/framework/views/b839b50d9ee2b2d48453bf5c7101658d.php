f90d344794f9328134b1fed042688106584ec8e1
humairasyafaira <117334650+humairasyafaira@users.noreply.github.com>
2024-11-03 16:34:37 +0700

    Video 14 - N + 1 Problem

---
+++ storage/framework/views/b839b50d9ee2b2d48453bf5c7101658d.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title> halaman about </title>
+
+</head>
+<body>
+
+  <h1>halaman about</h1>
+  
+
+</body>
+</html>
+<?php /**PATH D:\Herd\laravel11\resources\views/about.blade.php ENDPATH**/ ?>
